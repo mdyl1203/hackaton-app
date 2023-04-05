08c0c014f6f8c91ddd33c9359591083eb5158e4e
mdy2 <glpat-fCWexe1CEskkxurJmh2s>
2023-04-05 15:46:47 +0200

    test2

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
+import base64
 from flask import Flask, jsonify
-from jwcrypto import jwk
-
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 app = Flask(__name__)
 
-
 @app.route('/.well-known/jwks.json')
 def jwks():
-    with open('public_key.pem', 'rb') as f:
-        public_key_pem = f.read()
-
-    # Convert PEM keys to JWK format
-    public_key_jwk = jwk.JWK.from_pem(public_key_pem)
-
-    # Create JWKS dictionary
-    jwks = {
-        "keys": [
-            public_key_jwk.export(as_dict=True)
-        ]
-    }
-
-    return jsonify(jwks)
+    with open('public_key.pem', 'rb') as key_file:
+        public_key = serialization.load_pem_public_key(key_file.read(), backend=default_backend())
+        n_bytes = public_key.public_numbers().n.to_bytes((public_key.key_size + 7) // 8, byteorder='big')
+        e_bytes = public_key.public_numbers().e.to_bytes((public_key.key_size + 7) // 8, byteorder='big')
+        jwk = {
+            'kty': 'RSA',
+            'kid': 'my_key',
+            'use': 'sig',
+            'alg': 'RS256',
+            'n': base64.urlsafe_b64encode(n_bytes).decode('utf-8').rstrip('='),
+            'e': base64.urlsafe_b64encode(e_bytes).decode('utf-8').rstrip('=')
+        }
+        jwks = {
+            'keys': [jwk]
+        }
+        return jsonify(jwks)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
