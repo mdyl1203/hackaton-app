a109a02987e306bfa0c2fa69dd04483dabc20765
mdy2 <glpat-fCWexe1CEskkxurJmh2s>
2023-04-05 11:49:25 +0200

    test

--- main.py
+++ main.py
@@ -1,64 +1,43 @@
-from flask import Flask, jsonify
-import jwt
+from flask import Flask, request, jsonify
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
+import jwt
+import base64
 
 app = Flask(__name__)
 
-# Generate RSA key pair
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
-)
+# Generate an RSA key pair
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
 public_key = private_key.public_key()
 
-# Convert keys to JWK format
-private_jwk = {
-    'kty': 'RSA',
-    'kid': 'my-key-id',
-    'use': 'sig',
-    'alg': 'RS256',
-    'n': public_key.public_numbers().n,
-    'e': public_key.public_numbers().e,
-    'd': private_key.private_numbers().d,
-    'p': private_key.private_numbers().p,
-    'q': private_key.private_numbers().q,
-    'dp': private_key.private_numbers().dmp1,
-    'dq': private_key.private_numbers().dmq1,
-    'qi': private_key.private_numbers().iqmp
-}
-public_jwk = {
-    'kty': 'RSA',
-    'kid': 'my-key-id',
-    'use': 'sig',
-    'alg': 'RS256',
-    'n': public_key.public_numbers().n,
-    'e': public_key.public_numbers().e
+# Convert the public key to a JWK
+jwk = {
+    "kty": "RSA",
+    "kid": "my-key-id",
+    "use": "sig",
+    "alg": "RS256",
+    "n": base64.urlsafe_b64encode(public_key.public_numbers().n.to_bytes(256, 'big')).decode().rstrip('='),
+    "e": base64.urlsafe_b64encode(public_key.public_numbers().e.to_bytes(3, 'big')).decode().rstrip('=')
 }
 
 
-# Endpoint to get JWKs
+# Define a route for the JWKS endpoint
 @app.route('/jwks.json')
 def jwks():
-    return jsonify({
-        'keys': [public_jwk]
-    })
+    return jsonify({'keys': [jwk]})
 
 
-# Endpoint to generate and sign JWT
-@app.route('/jwt')
-def jwt_endpoint():
-    payload = {'sub': '1234567890', 'name': 'John Doe'}
-    encoded_jwt = jwt.encode(payload, private_key, algorithm='RS256')
-    return encoded_jwt
+# Define a route for the protected resource
+@app.route('/protected')
+def protected():
+    token = request.headers.get('Authorization').split(' ')[1]
+    decoded_token = jwt.decode(token, public_key, algorithms=['RS256'])
+    return jsonify(decoded_token)
 
 
-# Create a server status endpoint
 @app.route('/status')
 def status():
     return jsonify({'status': 'alive'})
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
